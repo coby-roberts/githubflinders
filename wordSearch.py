1f47f0ed8cbc1578878c49af1782beb58cc3a819
coby-roberts <104502547+coby-roberts@users.noreply.github.com>
2023-07-26 19:36:54 +0930

    Update wordSearch.py

--- wordSearch.py
+++ wordSearch.py
@@ -22,3 +22,19 @@ def word_search(doc_list, keyword):
         if keyword.lower() in normalized:
             indices.append(i)
     return indices
+
+def multi_word_search(documents, keywords):
+    """
+    Takes list of documents (each document is a string) and a list of keywords.  
+    Returns a dictionary where each key is a keyword, and the value is a list of indices
+    (from doc_list) of the documents containing that keyword
+
+    >>> doc_list = ["The Learn Python Challenge Casino.", "They bought a car and a casino", "Casinoville"]
+    >>> keywords = ['casino', 'they']
+    >>> multi_word_search(doc_list, keywords)
+    {'casino': [0, 1], 'they': [1]}
+    """
+    keyword_to_indices = {}
+    for keyword in keywords:
+        keyword_to_indices[keyword] = word_search(documents, keyword)
+    return keyword_to_indices
